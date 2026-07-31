392a90f2f741321fa837ccba490e9ab5d9ddf255
Duncan <mofiamadiback@gmail.com>
2026-07-31 04:41:33 +0300

    update DashboardCard.jsx and Dashboard.jsx files

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -1,57 +1,22 @@
-function DashboardCard({
+function DashboardCard({ 
   title,
   value,
   color,
+  icon
 }) {
   return (
-    <div className="row g-4">
-
-  <div className="col-md-4">
-    <div className="card border-0 shadow bg-primary text-white">
-      <div className="card-body">
+    <div className="card border-0 shadow h-100">
+      <div className={`card-body bg-${color} text-white rounded`}>
         <div className="d-flex justify-content-between align-items-center">
           <div>
-            <h6>Total Courses</h6>
-            <h2>{stats.totalCourses}</h2>
+            <h6>{title}</h6>
+            <h2>{value}</h2>
           </div>
 
-          <i className="bi bi-book-fill fs-1"></i>
+          <i className={`${icon} fs-1`}></i>
         </div>
       </div>
     </div>
-  </div>
-
-  <div className="col-md-4">
-    <div className="card border-0 shadow bg-success text-white">
-      <div className="card-body">
-        <div className="d-flex justify-content-between align-items-center">
-          <div>
-            <h6>Departments</h6>
-            <h2>{stats.totalDepartments}</h2>
-          </div>
-
-          <i className="bi bi-building fs-1"></i>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div className="col-md-4">
-    <div className="card border-0 shadow bg-warning text-dark">
-      <div className="card-body">
-        <div className="d-flex justify-content-between align-items-center">
-          <div>
-            <h6>Enrollments</h6>
-            <h2>{stats.totalEnrollments}</h2>
-          </div>
-
-          <i className="bi bi-person-check-fill fs-1"></i>
-        </div>
-      </div>
-    </div>
-  </div>
-
-</div>
   );
 }
 
